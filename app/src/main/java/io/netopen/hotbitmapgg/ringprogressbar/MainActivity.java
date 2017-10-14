d87a83ae2b6ac0b080aff938562f1c256255d8f0
skynetlabz <skynetlabz@gmail.com>
2017-10-15 00:36:07 +0500

    Added variable to toggle %age symbol display. Updated To Support Oreo

--- app/src/main/java/io/netopen/hotbitmapgg/ringprogressbar/MainActivity.java
+++ app/src/main/java/io/netopen/hotbitmapgg/ringprogressbar/MainActivity.java
@@ -1,84 +1,85 @@
 package io.netopen.hotbitmapgg.ringprogressbar;
 
-import io.netopen.hotbitmapgg.library.view.RingProgressBar;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 
+import io.netopen.hotbitmapgg.library.view.RingProgressBar;
+
 public class MainActivity extends AppCompatActivity {
 
-  private RingProgressBar mRingProgressBar1;
+    private RingProgressBar mRingProgressBar1;
 
-  private RingProgressBar mRingProgressBar2;
+    private RingProgressBar mRingProgressBar2;
 
-  private int progress = 0;
+    private int progress = 0;
 
-  private Handler mHandler = new Handler() {
+    private Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            if (msg.what == 0) {
+                if (progress < 100) {
+                    progress++;
+                    mRingProgressBar1.setProgress(progress);
+                    mRingProgressBar2.setProgress(progress);
+                    mRingProgressBar1.setOnProgressListener(new RingProgressBar.OnProgressListener() {
+
+                        @Override
+                        public void progressToComplete() {
+                            // Here after the completion of the processing
+                        }
+                    });
+
+                    mRingProgressBar2.setOnProgressListener(new RingProgressBar.OnProgressListener() {
+
+                        @Override
+                        public void progressToComplete() {
+                            // Here after the completion of the processing
+                        }
+                    });
+                }
+            }
+        }
+    };
+
 
     @Override
-    public void handleMessage(Message msg) {
+    protected void onCreate(Bundle savedInstanceState) {
 
-      if (msg.what == 0) {
-        if (progress < 100) {
-          progress++;
-          mRingProgressBar1.setProgress(progress);
-          mRingProgressBar2.setProgress(progress);
-          mRingProgressBar1.setOnProgressListener(new RingProgressBar.OnProgressListener() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            @Override
-            public void progressToComplete() {
-              // Here after the completion of the processing
-            }
-          });
+        mRingProgressBar1 = (RingProgressBar) findViewById(R.id.progress_bar_1);
+        mRingProgressBar2 = (RingProgressBar) findViewById(R.id.progress_bar_2);
+//        mRingProgressBar2.show_symbol = false;
 
-          mRingProgressBar2.setOnProgressListener(new RingProgressBar.OnProgressListener() {
+        new Thread(new Runnable() {
 
             @Override
-            public void progressToComplete() {
-              // Here after the completion of the processing
+            public void run() {
+
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        Thread.sleep(100);
+
+                        mHandler.sendEmptyMessage(0);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-          });
-        }
-      }
+        }).start();
     }
-  };
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onDestroy() {
 
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+        super.onDestroy();
 
-    mRingProgressBar1 = (RingProgressBar) findViewById(R.id.progress_bar_1);
-    mRingProgressBar2 = (RingProgressBar) findViewById(R.id.progress_bar_2);
-
-    new Thread(new Runnable() {
-
-      @Override
-      public void run() {
-
-        for (int i = 0; i < 100; i++) {
-          try {
-            Thread.sleep(100);
-
-            mHandler.sendEmptyMessage(0);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    }).start();
-  }
-
-
-  @Override
-  protected void onDestroy() {
-
-    super.onDestroy();
-
-    mHandler.removeCallbacksAndMessages(null);
-  }
+        mHandler.removeCallbacksAndMessages(null);
+    }
 }
--- library/src/main/java/io/netopen/hotbitmapgg/library/view/RingProgressBar.java
+++ library/src/main/java/io/netopen/hotbitmapgg/library/view/RingProgressBar.java
@@ -1,7 +1,5 @@
 package io.netopen.hotbitmapgg.library.view;
 
-import io.netopen.hotbitmapgg.library.R;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -13,6 +11,8 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
 
+import io.netopen.hotbitmapgg.library.R;
+
 /**
  * Created by hcc on 16/7/13 19:54
  * 100332338@qq.com
@@ -22,401 +22,413 @@ import android.view.View;
  */
 public class RingProgressBar extends View {
 
-  //画笔对象
-  private Paint paint;
+    //画笔对象
+    private Paint paint;
 
-  //View宽度
-  private int width;
+    //View宽度
+    private int width;
 
-  //View高度
-  private int height;
+    //View高度
+    private int height;
 
-  //默认宽高值
-  private int result = 0;
+    //默认宽高值
+    private int result = 0;
 
-  //默认padding值
-  private int padding = 0;
+    //默认padding值
+    private int padding = 0;
 
-  //圆环的颜色
-  private int ringColor;
+    //圆环的颜色
+    private int ringColor;
 
-  //圆环进度颜色
-  private int ringProgressColor;
+    //圆环进度颜色
+    private int ringProgressColor;
 
-  //文字颜色
-  private int textColor;
+    //文字颜色
+    private int textColor;
 
-  //文字大小
-  private float textSize;
+    //文字大小
+    private float textSize;
 
-  //圆环宽度
-  private float ringWidth;
+    //圆环宽度
+    private float ringWidth;
 
-  //最大值
-  private int max;
+    //最大值
+    private int max;
 
-  //进度值
-  private int progress;
+    //进度值
+    private int progress;
 
-  //是否显示文字
-  private boolean textIsShow;
+    //是否显示文字
+    private boolean textIsShow;
 
-  //圆环进度条的样式
-  private int style;
+    //圆环进度条的样式
+    private int style;
 
-  //空心样式
-  public static final int STROKE = 0;
+    //空心样式
+    public static final int STROKE = 0;
 
-  //实心样式
-  public static final int FILL = 1;
+    //实心样式
+    public static final int FILL = 1;
 
-  //进度回调接口
-  private OnProgressListener mOnProgressListener;
+    //to show % symbol
+    public static boolean show_symbol = true;
 
-  // 圆环中心
-  private int centre;
+    //进度回调接口
+    private OnProgressListener mOnProgressListener;
 
-  // 圆环半径
-  private int radius;
+    // 圆环中心
+    private int centre;
 
-
-  public RingProgressBar(Context context) {
-
-    this(context, null);
-  }
+    // 圆环半径
+    private int radius;
 
 
-  public RingProgressBar(Context context, AttributeSet attrs) {
+    public RingProgressBar(Context context) {
 
-    this(context, attrs, 0);
-  }
-
-
-  public RingProgressBar(Context context, AttributeSet attrs, int defStyle) {
-
-    super(context, attrs, defStyle);
-
-    //初始化画笔
-    paint = new Paint();
-
-    //初始化默认宽高值
-    result = dp2px(100);
-
-    //初始化属性
-    TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.RingProgressBar);
-
-    ringColor = mTypedArray.getColor(R.styleable.RingProgressBar_ringColor, Color.BLACK);
-    ringProgressColor = mTypedArray.getColor(R.styleable.RingProgressBar_ringProgressColor,
-        Color.WHITE);
-    textColor = mTypedArray.getColor(R.styleable.RingProgressBar_textColor, Color.BLACK);
-    textSize = mTypedArray.getDimension(R.styleable.RingProgressBar_textSize, 16);
-    ringWidth = mTypedArray.getDimension(R.styleable.RingProgressBar_ringWidth, 5);
-    max = mTypedArray.getInteger(R.styleable.RingProgressBar_max, 100);
-    textIsShow = mTypedArray.getBoolean(R.styleable.RingProgressBar_textIsShow, true);
-    style = mTypedArray.getInt(R.styleable.RingProgressBar_style, 0);
-
-    mTypedArray.recycle();
-  }
-
-
-  @SuppressLint("DrawAllocation")
-  @Override
-  protected void onDraw(Canvas canvas) {
-
-    super.onDraw(canvas);
-
-    centre = getWidth() / 2;
-    radius = (int) (centre - ringWidth / 2);
-
-    //绘制外层圆
-    drawCircle(canvas);
-    //绘制文本内容
-    drawTextContent(canvas);
-    //绘制进度条
-    drawProgress(canvas);
-  }
-
-
-  /**
-   * 绘制外层圆环
-   */
-  private void drawCircle(Canvas canvas) {
-    //设置画笔颜色
-    paint.setColor(ringColor);
-    //设置画笔样式
-    paint.setStyle(Paint.Style.STROKE);
-    //设置stroke的宽度
-    paint.setStrokeWidth(ringWidth);
-    //设置抗锯齿
-    paint.setAntiAlias(true);
-    //绘制圆形
-    canvas.drawCircle(centre, centre, radius, paint);
-  }
-
-
-  /**
-   * 绘制进度文本
-   */
-  private void drawTextContent(Canvas canvas) {
-    //设置stroke的宽度
-    paint.setStrokeWidth(0);
-    //设置文字的颜色
-    paint.setColor(textColor);
-    //设置文字的大小
-    paint.setTextSize(textSize);
-    //设置文字的style
-    paint.setTypeface(Typeface.DEFAULT);
-    //设置进度值
-    int percent = (int) (((float) progress / (float) max) * 100);
-    //获取文字的宽度 用于绘制文本内容
-    float textWidth = paint.measureText(percent + "%");
-    //绘制文本 会根据设置的是否显示文本的属性&是否是Stroke的样式进行判断
-    if (textIsShow && percent != 0 && style == STROKE) {
-      canvas.drawText(percent + "%", centre - textWidth / 2, centre + textSize / 2, paint);
+        this(context, null);
     }
-  }
 
 
-  /**
-   * 绘制进度条
-   */
-  private void drawProgress(Canvas canvas) {
-    //绘制进度 根据设置的样式进行绘制
-    paint.setStrokeWidth(ringWidth);
-    paint.setColor(ringProgressColor);
+    public RingProgressBar(Context context, AttributeSet attrs) {
 
-    //Stroke样式
-    RectF strokeOval = new RectF(centre - radius, centre - radius, centre + radius,
-        centre + radius);
-    //FIll样式
-    RectF fillOval = new RectF(centre - radius + ringWidth + padding,
-        centre - radius + ringWidth + padding, centre + radius - ringWidth - padding,
-        centre + radius - ringWidth - padding);
+        this(context, attrs, 0);
+    }
 
-    switch (style) {
-      case STROKE: {
+
+    public RingProgressBar(Context context, AttributeSet attrs, int defStyle) {
+
+        super(context, attrs, defStyle);
+
+        //初始化画笔
+        paint = new Paint();
+
+        //初始化默认宽高值
+        result = dp2px(100);
+
+        //初始化属性
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.RingProgressBar);
+
+        ringColor = mTypedArray.getColor(R.styleable.RingProgressBar_ringColor, Color.BLACK);
+        ringProgressColor = mTypedArray.getColor(R.styleable.RingProgressBar_ringProgressColor,
+                Color.WHITE);
+        textColor = mTypedArray.getColor(R.styleable.RingProgressBar_textColor, Color.BLACK);
+        textSize = mTypedArray.getDimension(R.styleable.RingProgressBar_textSize, 16);
+        ringWidth = mTypedArray.getDimension(R.styleable.RingProgressBar_ringWidth, 5);
+        max = mTypedArray.getInteger(R.styleable.RingProgressBar_max, 100);
+        textIsShow = mTypedArray.getBoolean(R.styleable.RingProgressBar_textIsShow, true);
+        style = mTypedArray.getInt(R.styleable.RingProgressBar_style, 0);
+
+        mTypedArray.recycle();
+    }
+
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        super.onDraw(canvas);
+
+        centre = getWidth() / 2;
+        radius = (int) (centre - ringWidth / 2);
+
+        //绘制外层圆
+        drawCircle(canvas);
+        //绘制进度条
+        drawProgress(canvas);
+        //绘制文本内容
+        drawTextContent(canvas);
+    }
+
+
+    /**
+     * 绘制外层圆环
+     */
+    private void drawCircle(Canvas canvas) {
+        //设置画笔颜色
+        paint.setColor(ringColor);
+        //设置画笔样式
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        canvas.drawArc(strokeOval, -90, 360 * progress / max, false, paint);
-        break;
-      }
-      case FILL: {
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        if (progress != 0) {
-          canvas.drawArc(fillOval, -90, 360 * progress / max, true, paint);
+        //设置stroke的宽度
+        paint.setStrokeWidth(ringWidth);
+        //设置抗锯齿
+        paint.setAntiAlias(true);
+        //绘制圆形
+        canvas.drawCircle(centre, centre, radius, paint);
+    }
+
+
+    /**
+     * 绘制进度文本
+     */
+    private void drawTextContent(Canvas canvas) {
+        //设置stroke的宽度
+        paint.setStrokeWidth(0);
+        //设置文字的颜色
+        paint.setColor(textColor);
+        //设置文字的大小
+        paint.setTextSize(textSize);
+        //设置文字的style
+        paint.setTypeface(Typeface.DEFAULT);
+        //设置进度值
+        int percent = (int) (((float) progress / (float) max) * 100);
+        //获取文字的宽度 用于绘制文本内容
+        float textWidth;
+        //width if % symbol is used or not
+        if (show_symbol)
+            textWidth = paint.measureText(percent + "%");
+        else
+            textWidth = paint.measureText(percent + "");
+        //绘制文本 会根据设置的是否显示文本的属性&是否是Stroke的样式进行判断
+        if (textIsShow && percent != 0 && show_symbol && style == STROKE) {
+            canvas.drawText(percent + "%", centre - textWidth / 2, centre + textSize / 2, paint);
+        } else if (textIsShow && percent != 0 && style == FILL) {
+            canvas.drawText(percent + "%", centre - textWidth / 2, centre + textSize / 2, paint);
+        } else if (textIsShow && percent != 0 && !show_symbol && style == STROKE) {
+            canvas.drawText(percent + "", centre - textWidth / 2, centre + textSize / 2, paint);
         }
-        break;
-      }
-    }
-  }
-
-
-  @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    //获取宽高的mode和size
-    int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-    int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-    int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-    int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-
-    //测量宽度
-    if (widthMode == MeasureSpec.AT_MOST) {
-      width = result;
-    } else {
-      width = widthSize;
     }
 
-    //测量高度
-    if (heightMode == MeasureSpec.AT_MOST) {
-      height = result;
-    } else {
-      height = heightSize;
+
+    /**
+     * 绘制进度条
+     */
+    private void drawProgress(Canvas canvas) {
+        //绘制进度 根据设置的样式进行绘制
+        paint.setStrokeWidth(ringWidth);
+        paint.setColor(ringProgressColor);
+
+        //Stroke样式
+        RectF strokeOval = new RectF(centre - radius, centre - radius, centre + radius,
+                centre + radius);
+        //FIll样式
+        RectF fillOval = new RectF(centre - radius + ringWidth + padding,
+                centre - radius + ringWidth + padding, centre + radius - ringWidth - padding,
+                centre + radius - ringWidth - padding);
+
+        switch (style) {
+            case STROKE: {
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setStrokeCap(Paint.Cap.ROUND);
+                canvas.drawArc(strokeOval, -90, 360 * progress / max, false, paint);
+                break;
+            }
+            case FILL: {
+                paint.setStyle(Paint.Style.FILL_AND_STROKE);
+                paint.setStrokeCap(Paint.Cap.ROUND);
+                if (progress != 0) {
+                    canvas.drawArc(fillOval, -90, 360 * progress / max, true, paint);
+                }
+                break;
+            }
+        }
     }
 
-    //设置测量的宽高值
-    setMeasuredDimension(width, height);
-  }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-  @Override
-  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //获取宽高的mode和size
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-    super.onSizeChanged(w, h, oldw, oldh);
-    //确定View的宽高
-    width = w;
-    height = h;
-    //初始化padding值 默认设置为5
-    padding = dp2px(5);
-  }
+        //测量宽度
+        if (widthMode == MeasureSpec.AT_MOST) {
+            width = result;
+        } else {
+            width = widthSize;
+        }
 
+        //测量高度
+        if (heightMode == MeasureSpec.AT_MOST) {
+            height = result;
+        } else {
+            height = heightSize;
+        }
 
-  /**
-   * 获取当前的最大进度值
-   */
-  public synchronized int getMax() {
-
-    return max;
-  }
-
-
-  /**
-   * 设置最大进度值
-   */
-  public synchronized void setMax(int max) {
-
-    if (max < 0) {
-      throw new IllegalArgumentException("The max progress of 0");
+        //设置测量的宽高值
+        setMeasuredDimension(width, height);
     }
-    this.max = max;
-  }
 
 
-  /**
-   * 获取进度值
-   */
-  public synchronized int getProgress() {
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 
-    return progress;
-  }
-
-
-  /**
-   * 设置进度值 根据进度值进行View的重绘刷新进度
-   */
-  public synchronized void setProgress(int progress) {
-
-    if (progress < 0) {
-      throw new IllegalArgumentException("The progress of 0");
+        super.onSizeChanged(w, h, oldw, oldh);
+        //确定View的宽高
+        width = w;
+        height = h;
+        //初始化padding值 默认设置为5
+        padding = dp2px(5);
     }
-    if (progress > max) {
-      progress = max;
+
+
+    /**
+     * 获取当前的最大进度值
+     */
+    public synchronized int getMax() {
+
+        return max;
     }
-    if (progress <= max) {
-      this.progress = progress;
-      postInvalidate();
+
+
+    /**
+     * 设置最大进度值
+     */
+    public synchronized void setMax(int max) {
+
+        if (max < 0) {
+            throw new IllegalArgumentException("The max progress of 0");
+        }
+        this.max = max;
     }
-    if (progress == max) {
-      if (mOnProgressListener != null) {
-        mOnProgressListener.progressToComplete();
-      }
+
+
+    /**
+     * 获取进度值
+     */
+    public synchronized int getProgress() {
+
+        return progress;
     }
-  }
 
 
-  /**
-   * 获取圆环的颜色
-   */
-  public int getRingColor() {
+    /**
+     * 设置进度值 根据进度值进行View的重绘刷新进度
+     */
+    public synchronized void setProgress(int progress) {
 
-    return ringColor;
-  }
-
-
-  /**
-   * 设置圆环的颜色
-   */
-  public void setRingColor(int ringColor) {
-
-    this.ringColor = ringColor;
-  }
-
-
-  /**
-   * 获取圆环进度的颜色
-   */
-  public int getRingProgressColor() {
-
-    return ringProgressColor;
-  }
+        if (progress < 0) {
+            throw new IllegalArgumentException("The progress of 0");
+        }
+        if (progress > max) {
+            progress = max;
+        }
+        if (progress <= max) {
+            this.progress = progress;
+            postInvalidate();
+        }
+        if (progress == max) {
+            if (mOnProgressListener != null) {
+                mOnProgressListener.progressToComplete();
+            }
+        }
+    }
 
 
-  /**
-   * 设置圆环进度的颜色
-   */
-  public void setRingProgressColor(int ringProgressColor) {
+    /**
+     * 获取圆环的颜色
+     */
+    public int getRingColor() {
 
-    this.ringProgressColor = ringProgressColor;
-  }
-
-
-  /**
-   * 获取文字的颜色
-   */
-  public int getTextColor() {
-
-    return textColor;
-  }
+        return ringColor;
+    }
 
 
-  /**
-   * 设置文字颜色
-   */
-  public void setTextColor(int textColor) {
+    /**
+     * 设置圆环的颜色
+     */
+    public void setRingColor(int ringColor) {
 
-    this.textColor = textColor;
-  }
-
-
-  /**
-   * 获取文字的大小
-   */
-  public float getTextSize() {
-
-    return textSize;
-  }
+        this.ringColor = ringColor;
+    }
 
 
-  /**
-   * 设置文字的大小
-   */
-  public void setTextSize(float textSize) {
+    /**
+     * 获取圆环进度的颜色
+     */
+    public int getRingProgressColor() {
 
-    this.textSize = textSize;
-  }
-
-
-  /**
-   * 获取圆环的宽度
-   */
-  public float getRingWidth() {
-
-    return ringWidth;
-  }
+        return ringProgressColor;
+    }
 
 
-  /**
-   * 设置圆环的宽度
-   */
-  public void setRingWidth(float ringWidth) {
+    /**
+     * 设置圆环进度的颜色
+     */
+    public void setRingProgressColor(int ringProgressColor) {
 
-    this.ringWidth = ringWidth;
-  }
-
-
-  /**
-   * dp转px
-   */
-  public int dp2px(int dp) {
-
-    float density = getContext().getResources().getDisplayMetrics().density;
-    return (int) (dp * density + 0.5f);
-  }
+        this.ringProgressColor = ringProgressColor;
+    }
 
 
-  /**
-   * 进度完成回调接口
-   */
-  public interface OnProgressListener {
+    /**
+     * 获取文字的颜色
+     */
+    public int getTextColor() {
 
-    void progressToComplete();
-  }
+        return textColor;
+    }
 
 
-  public void setOnProgressListener(OnProgressListener mOnProgressListener) {
+    /**
+     * 设置文字颜色
+     */
+    public void setTextColor(int textColor) {
 
-    this.mOnProgressListener = mOnProgressListener;
-  }
+        this.textColor = textColor;
+    }
+
+
+    /**
+     * 获取文字的大小
+     */
+    public float getTextSize() {
+
+        return textSize;
+    }
+
+
+    /**
+     * 设置文字的大小
+     */
+    public void setTextSize(float textSize) {
+
+        this.textSize = textSize;
+    }
+
+
+    /**
+     * 获取圆环的宽度
+     */
+    public float getRingWidth() {
+
+        return ringWidth;
+    }
+
+
+    /**
+     * 设置圆环的宽度
+     */
+    public void setRingWidth(float ringWidth) {
+
+        this.ringWidth = ringWidth;
+    }
+
+
+    /**
+     * dp转px
+     */
+    public int dp2px(int dp) {
+
+        float density = getContext().getResources().getDisplayMetrics().density;
+        return (int) (dp * density + 0.5f);
+    }
+
+
+    /**
+     * 进度完成回调接口
+     */
+    public interface OnProgressListener {
+
+        void progressToComplete();
+    }
+
+
+    public void setOnProgressListener(OnProgressListener mOnProgressListener) {
+
+        this.mOnProgressListener = mOnProgressListener;
+    }
 }
